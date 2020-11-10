662c4b1fd0af879d952b97e870f6bc296c308c69
Joshy <josh.ludahl@gmail.com>
2020-11-09 16:48:52 -0800

    Sesh

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
+
+app.config.update(
+    SESSION_PERMANENT=False
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Strict',
+)
 
 Session(app)
 
