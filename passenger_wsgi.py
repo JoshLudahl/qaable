8c438f24ea9143851c602c40e53904ac3ba7de26
Joshy <josh.ludahl@gmail.com>
2020-11-09 16:47:07 -0800

    play

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,11 +8,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
 
-app.config.update(
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Strict',
-)
 Session(app)
 
 import test
