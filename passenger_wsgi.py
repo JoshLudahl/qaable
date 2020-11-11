87e9a0cdb34fb437524acab0a035d648179a7a98
Joshy <josh.ludahl@gmail.com>
2020-11-10 21:56:55 -0800

    wea

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -23,12 +23,12 @@ def getWeather():
     api_key = os.environ.get("WEATHER_API_KEY")
     base_url = "https://api.openweathermap.org/data/2.5/onecall?lat=45.4187&lon=122.7872&units=imperial&appid=" + api_key
     http_request = requests.get(base_url)
-    return http_request.json()
+    return http_request
 
 @app.route('/')
 def hello_world():
     weather=getWeather()
-    res = make_response(render_template('index.html', weather=weather, 200)
+    res = make_response(render_template('index.html', weather=weather.json(), 200)
     return res
 
 @app.route('/', methods = ["POST"])
@@ -36,7 +36,7 @@ def postHello():
     session["name"] = request.form.get("name")
     req = request.form.get("name", "world")
     weather = getWeather()
-    res = make_response(render_template('index.html', named=req, weather=weather, 200)
+    res = make_response(render_template('index.html', named=req, weather=weather.json(), 200)
     return res
 
 application = app
