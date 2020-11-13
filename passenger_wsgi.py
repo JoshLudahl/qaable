f4236640dc122aed1c33247f95c5e9fc792e2c5a
Joshy <josh.ludahl@gmail.com>
2020-11-12 18:41:18 -0800

    wea

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -50,6 +50,6 @@ def postHello():
     )
 @app.template_filter('datetimeformat')
 def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
-    return value.strftime(format)
+    return 34
 
 application = app
