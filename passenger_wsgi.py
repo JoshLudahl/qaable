adc24bf4cbce4224768ddcfa381c0c45816560d4
Joshy <josh.ludahl@gmail.com>
2020-11-12 18:31:49 -0800

    date

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -48,5 +48,12 @@ def postHello():
         ), 
         200
     )
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    date = dateutil.parser.parse(date)
+    native = date.replace(tzinfo=None)
+    format='%b %d, %Y'
+    return native.strftime(format) 
     
 application = app
