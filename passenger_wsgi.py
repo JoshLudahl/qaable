56a037d04da8b883d782a13092b2f036f97a3324
Joshy <josh.ludahl@gmail.com>
2020-11-22 14:25:55 -0800

    icon updates

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -70,11 +70,28 @@ def datetimeformat(value, offset):
     return dt.fromtimestamp(value + offset).strftime("%A")
 
 weather_icons = {
-    #list of icon options
+    '01n': "fa-moon",
+    '01d': "fa-sun",
+    '02n': "fa-cloud-moon",
+    '02d': "fa-cloud-sun",
+    '03n': "fa-cloud-moon",
+    '03d': "fa-cloud-sun",
+    '04n': "fa-cloud-moon",
+    '04d': "fa-cloud-sun",
+    '09n': "fa-cloud",
+    '09d': "fa-cloud",
+    '10n': "fa-cloud-rain",
+    '10d': "fa-cloud-rain",
+    '11n': "fa-bolt",
+    '11d': "fa-bolt",
+    '13n': "fa-snowflake",
+    '13d': "fa-snowflake",
+    '50n': "fa-smog",
+    '50d': "fa-smog",
 }
 
 @app.template_filter('weather_icon_filter')
 def weather_icon_filter(value):
-    return "fa-cloud"
+    return weather_icons[value]
 
 application = app
