41c284035c7a8aa11e4a194249b8a55c5037d91c
Joshy <josh.ludahl@gmail.com>
2020-11-22 14:46:40 -0800

    moved

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -17,7 +17,7 @@ app.config.update(
 
 Session(app)
 
-import test
+import test, weather_dict
 
 def getWeather(lat = 45.445033, lon = -122.793760):
     api_key = os.environ.get("WEATHER_API_KEY")
@@ -68,27 +68,6 @@ def postHello():
 @app.template_filter('datetimeformat')
 def datetimeformat(value, offset):
     return dt.fromtimestamp(value + offset).strftime("%A")
-
-weather_icons = {
-    '01n': "fa-moon",
-    '01d': "fa-sun",
-    '02n': "fa-cloud-moon",
-    '02d': "fa-cloud-sun",
-    '03n': "fa-cloud-moon",
-    '03d': "fa-cloud-sun",
-    '04n': "fa-cloud-moon",
-    '04d': "fa-cloud-sun",
-    '09n': "fa-cloud",
-    '09d': "fa-cloud",
-    '10n': "fa-cloud-rain",
-    '10d': "fa-cloud-rain",
-    '11n': "fa-bolt",
-    '11d': "fa-bolt",
-    '13n': "fa-snowflake",
-    '13d': "fa-snowflake",
-    '50n': "fa-smog",
-    '50d': "fa-smog",
-}
 
 @app.template_filter('weather_icon_filter')
 def weather_icon_filter(value):
